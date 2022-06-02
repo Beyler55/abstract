f5d0ce417824132ba787792da0f969ccee29b584
Beyler55 <baylarss@code.edu.az>
2022-06-02 17:28:43 +0400

    abstract -interface

---
+++ abstraction/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User
+            {
+                Email = "Baylar@code",
+                FullName = "Baylar Sadiqov"
+            };
+            user.ShowInfo();
+
+        }
+
+    }
+}
+
---
+++ abstraction/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace abstraction
+{
+    class User : IAccount
+    {
+
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User()
+        {
+            do
+            {
+                Console.WriteLine("Sifreni daxil edin:");
+                Password = Console.ReadLine();
+            }
+            while (!PasswordChecker(Password));
+
+        }
+
+        public bool PasswordChecker(string password)
+        {
+            if (password.Length > 7)
+            {
+                foreach (var item in password)
+                {
+                    if (char.IsDigit(item))
+                    {
+                        return true;
+                    }
+                    if (password == password.ToUpper() || password == password.ToLower())
+                    {
+                        return false;
+                    }
+                }
+
+            }
+            return false;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(FullName);
+            Console.WriteLine(Email);
+        }
+    }
+}
